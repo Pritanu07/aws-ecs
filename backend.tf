ce5b3b9b2d9e1ed521ae8e4865992ded17f40f9c
shanmugapriya <shanmugapriya.rajendran@gmail.com>
2023-07-22 11:55:12 +0800

    ecs cluster

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce2-tfstate-bkt"
+    key    = "priya-ecs-1.tfstate"   #Change the value  of this to yourname-ecs-1.tfstate for  example
+    region = "ap-southeast-1"
+  }
+}
+
